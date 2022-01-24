664a61c437db24525e11e8f8c76fb5c45f2aa5b7
Dries Hugaerts <dries.hugaerts@live.be>
2022-01-24 15:01:58 +0100

    + Added yasgui interface + dispatcher config, + Added migrations service + config folder, + Added database `toLoad` folder, + Added docker-compose.dev.yml + updated docker-compose.yml accordingly - Removed resource service + config folder,

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,25 +1,32 @@
 defmodule Dispatcher do
   use Matcher
+
   define_accept_types [
     html: [ "text/html", "application/xhtml+html" ],
-    json: [ "application/json", "application/vnd.api+json" ]
+    json: [ "application/json", "application/vnd.api+json" ],
+    sparql: [ "application/sparql-results+json" ]
   ]
 
   @any %{}
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
+  @sparql %{ accept: %{ sparql: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  match "/sparql", @html do
+    forward conn, [], "http://yasgui:80"
+  end
+
+  match "/sparql", @sparql do
+    forward conn, [], "http://database:8890/sparql"
+  end
+
+  # Temporary Redirect to "/sparql" endpoint
+  match "/*_", @html do
+    send_resp( conn, 307, "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url='/sparql'\" /></head><body><p>Please follow <a href=\"/sparql\">this link</a>.</p></body></html>" )
+  end
 
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
+
 end
